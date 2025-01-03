b3c004023a80c0da9953b66e8c615726d8f7e46a
ArminMolnar <molnar.armin9@gmail.com>
2025-01-04 00:33:54 +0100

    worked on dept_order_procedure

--- Database/pkg_order_body.sql
+++ Database/pkg_order_body.sql
@@ -1,8 +1,8 @@
 CREATE OR REPLACE PACKAGE BODY pkg_order IS
   -- Warehouse coordinator orders from Supplier
   PROCEDURE warehouse_order_proc(p_wh_coordinator_id IN NUMBER
-                           ,p_product_id        IN NUMBER
-                           ,p_quantity          IN NUMBER) IS
+                                ,p_product_id        IN NUMBER
+                                ,p_quantity          IN NUMBER) IS
   
     v_supplier_id NUMBER;
   
@@ -28,9 +28,18 @@ CREATE OR REPLACE PACKAGE BODY pkg_order IS
       ,v_supplier_id
       ,p_product_id
       ,p_quantity
-      ,sysdate
+      ,SYSDATE
       ,'Ordered');
   
+    COMMIT;
+    dbms_output.put_line('Transaction committed (order from supplier)');
+  
+  EXCEPTION
+    WHEN OTHERS THEN
+      dbms_output.put_line('Error occurred: ' || SQLERRM);
+      ROLLBACK;
+      RAISE;
+    
   END warehouse_order_proc;
 
   -- Products arrive to the warehouse
@@ -56,42 +65,47 @@ CREATE OR REPLACE PACKAGE BODY pkg_order IS
           ,completion_date = SYSDATE
      WHERE wh_order_id = p_wh_order_id;
   
-    FOR i IN (SELECT dept_order_id
-                FROM department_order
-               WHERE product_id = v_product_id
-                 AND status = 'Ordered'
-                 FOR UPDATE NOWAIT)
-    LOOP
-      deliver_to_department(i.dept_order_id);
-    END LOOP;
+    COMMIT;
+    dbms_output.put_line('Transaction committed (products arrive to the warehouse');
   
+  EXCEPTION
+    WHEN OTHERS THEN
+      dbms_output.put_line('Error occurred: ' || SQLERRM);
+      ROLLBACK;
+      RAISE;
+    
   END deliver_to_warehouse;
-  
+
   -- Department orders from Warehouse coordinator
-  PROCEDURE department_order_proc(p_dept_id       IN NUMBER  
-                            ,p_wh_coordinator_id  IN NUMBER       
-                            ,p_product_id         IN NUMBER     
-                            ,p_quantity           IN NUMBER) IS  
-    v_available_quantity NUMBER;
+  PROCEDURE department_order_proc(p_dept_id           IN NUMBER
+                                 ,p_wh_coordinator_id IN NUMBER
+                                 ,p_product_id        IN NUMBER
+                                 ,p_quantity          IN NUMBER) IS
+    v_available_quantity    NUMBER;
     v_wh_coord_warehouse_id NUMBER;
-    v_warehouse_id NUMBER;
+    v_warehouse_id          NUMBER;
+    v_min_stock_level       NUMBER;
   
   BEGIN
-     SELECT wh.warehouse_coordinator_id
-       INTO v_wh_coord_warehouse_id
-       FROM warehouse wh
-      WHERE wh.warehouse_coordinator_id = p_wh_coordinator_id;
-     
   
-    SELECT warehouse_id ,stock_quantity
-      INTO v_warehouse_id, v_available_quantity
+    SELECT warehouse_id
+          ,stock_quantity
+          ,min_stock_level
+      INTO v_warehouse_id
+          ,v_available_quantity
+          ,v_min_stock_level
       FROM product
      WHERE product_id = p_product_id
        FOR UPDATE NOWAIT;
   
-    IF v_wh_coord_warehouse_id = v_warehouse_id AND 
-       v_available_quantity >= p_quantity 
-      
+    SELECT wh.warehouse_coordinator_id
+      INTO v_wh_coord_warehouse_id
+      FROM warehouse wh
+     WHERE wh.warehouse_coordinator_id = p_wh_coordinator_id;
+  
+    IF v_wh_coord_warehouse_id = p_wh_coordinator_id
+       AND v_available_quantity >= p_quantity
+    -- Enough products
     THEN
       INSERT INTO department_order
         (dept_order_id
@@ -109,7 +123,9 @@ CREATE OR REPLACE PACKAGE BODY pkg_order IS
         ,p_quantity
         ,SYSDATE
         ,'Ordered');
+    
     ELSE
+      -- Not enough products - v_available_quantity <= p_quantity
       INSERT INTO department_order
         (dept_order_id
         ,dept_id
@@ -126,33 +142,61 @@ CREATE OR REPLACE PACKAGE BODY pkg_order IS
         ,p_quantity
         ,SYSDATE
         ,'Ordered');
-      warehouse_order_proc(p_wh_coordinator_id, p_product_id, p_quantity);
+    
+      warehouse_order_proc(p_wh_coordinator_id,
+                           p_product_id,
+                           ((v_min_stock_level * 4) +
+                           (p_quantity - v_available_quantity)));
     END IF;
-  END department_order_proc;
   
+    COMMIT;
+    dbms_output.put_line('Transaction committed');
+  
+  EXCEPTION
+    WHEN OTHERS THEN
+      dbms_output.put_line('Error occurred: ' || SQLERRM);
+      ROLLBACK;
+      RAISE;
+  END department_order_proc;
+
   -- Products handed over to department
   PROCEDURE deliver_to_department(p_dept_order_id IN NUMBER) IS
     v_quantity   NUMBER;
     v_product_id NUMBER;
+    v_status     VARCHAR2(30);
   
   BEGIN
     SELECT quantity
           ,product_id
+          ,status
       INTO v_quantity
           ,v_product_id
+          ,v_status
       FROM department_order
      WHERE dept_order_id = p_dept_order_id
        FOR UPDATE NOWAIT;
   
-    UPDATE product
-       SET stock_quantity = stock_quantity - v_quantity
-     WHERE product_id = v_product_id;
-  
-    UPDATE department_order
-       SET status          = 'Completed'
-          ,completion_date = SYSDATE
-     WHERE dept_order_id = p_dept_order_id;
-  
+    IF v_status = 'Ordered'
+    THEN
+      UPDATE product
+         SET stock_quantity = stock_quantity - v_quantity
+       WHERE product_id = v_product_id;
+    
+      UPDATE department_order
+         SET status          = 'Completed'
+            ,completion_date = SYSDATE
+       WHERE dept_order_id = p_dept_order_id;
+    
+      COMMIT;
+      dbms_output.put_line('Order completed and stock updated');
+    ELSE
+      dbms_output.put_line('Order already processed or in invalid state');
+    END IF;
+  EXCEPTION
+    WHEN OTHERS THEN
+      dbms_output.put_line('Error occurred: ' || SQLERRM);
+      ROLLBACK;
+      RAISE;
   END deliver_to_department;
 
 END pkg_order;
