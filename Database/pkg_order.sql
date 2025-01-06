6174b6761a8d95dd99f824e62de0cfc36b664e12
ArminMolnar <molnar.armin9@gmail.com>
2025-01-06 01:16:06 +0100

    modified procedures + added function

--- Database/pkg_order.sql
+++ Database/pkg_order.sql
@@ -1,14 +1,15 @@
 CREATE OR REPLACE PACKAGE pkg_order IS
-  PROCEDURE warehouse_order_proc(p_wh_coordinator_id IN NUMBER
-                           ,p_product_id        IN NUMBER
-                           ,p_quantity          IN NUMBER);
+  PROCEDURE warehouse_order_proc(p_product_id IN NUMBER
+                                ,p_quantity   IN NUMBER);
 
   PROCEDURE deliver_to_warehouse(p_wh_order_id IN NUMBER);
-  
-  PROCEDURE department_order_proc(p_dept_id            IN NUMBER
-                            ,p_wh_coordinator_id IN NUMBER
-                            ,p_product_id         IN NUMBER
-                            ,p_quantity           IN NUMBER);
-                            
-    PROCEDURE deliver_to_department(p_dept_order_id IN NUMBER);
+
+  PROCEDURE department_order_proc(p_dept_id    IN NUMBER
+                                 ,p_product_id IN NUMBER
+                                 ,p_quantity   IN NUMBER);
+
+  PROCEDURE deliver_to_department(p_dept_order_id IN NUMBER);
+
+  FUNCTION list_active_dept_order(p_status IN VARCHAR2 DEFAULT 'Active')
+    RETURN VARCHAR2;
 END pkg_order;
