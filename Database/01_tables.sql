20e2553b31bbbb35b0dfea68a1e4ada16a6e38b5
ArminMolnar <molnar.armin9@gmail.com>
2025-01-06 14:18:29 +0100

    updated order of tables

--- Database/01_tables.sql
+++ Database/01_tables.sql
@@ -1,41 +1,3 @@
-CREATE TABLE product(
-       product_id NUMBER PRIMARY KEY,
-       supplier_id NUMBER NOT NULL,
-       warehouse_id NUMBER NOT NULL,
-       product_name VARCHAR2(30) NOT NULL,
-       product_category VARCHAR2(30) NOT NULL,
-       product_size VARCHAR2(20),
-       stock_quantity NUMBER NOT NULL, 
-       measurement_unit VARCHAR2(10),      
-       product_price NUMBER NOT NULL,
-       min_stock_level NUMBER,
-       max_stock_level NUMBER,
-       created_on DATE DEFAULT SYSDATE NOT NULL,
-       last_mod DATE,
-       mod_user VARCHAR2(100),
-       dml_flag VARCHAR2(1),
-       version NUMBER,
-       CONSTRAINT supplier_fk foreign key (supplier_id) references supplier(supplier_id),
-       CONSTRAINT warehouse_fk foreign key (warehouse_id) references warehouse(warehouse_id),
-       CONSTRAINT quantity_positive_check CHECK (stock_quantity > 0),
-       CONSTRAINT price_positive_check CHECK (product_price > 0),
-       CONSTRAINT min_stock_positive_check CHECK (min_stock_level > 0),
-       CONSTRAINT stock_level_check CHECK (max_stock_level > min_stock_level)
-       )TABLESPACE users;
-
-
-CREATE TABLE warehouse(
-       warehouse_id NUMBER PRIMARY KEY,
-       warehouse_name VARCHAR2(30) NOT NULL,       
-       warehouse_coordinator_id NUMBER NOT NULL,
-       created_on DATE DEFAULT SYSDATE NOT NULL,
-       last_mod DATE,
-       mod_user varchar2(100),
-       dml_flag VARCHAR2(1),
-       version NUMBER,
-       constraint wh_coordinator_fk foreign key (warehouse_coordinator_id) references wh_coordinator(wh_coordinator_id)   
-)TABLESPACE users;
-
 CREATE TABLE wh_coordinator(
        wh_coordinator_id NUMBER PRIMARY KEY,
        wh_coordinator_name VARCHAR2(50) NOT NULL,
@@ -47,6 +9,18 @@ CREATE TABLE wh_coordinator(
        mod_user varchar2(100),
        dml_flag VARCHAR2(1),
        version NUMBER       
+)TABLESPACE users;
+
+CREATE TABLE warehouse(
+       warehouse_id NUMBER PRIMARY KEY,
+       warehouse_name VARCHAR2(30) NOT NULL,       
+       warehouse_coordinator_id NUMBER NOT NULL,
+       created_on DATE DEFAULT SYSDATE NOT NULL,
+       last_mod DATE,
+       mod_user varchar2(100),
+       dml_flag VARCHAR2(1),
+       version NUMBER,
+       constraint wh_coordinator_fk foreign key (warehouse_coordinator_id) references wh_coordinator(wh_coordinator_id)   
 )TABLESPACE users;
 
 CREATE TABLE supplier(
@@ -78,6 +52,31 @@ CREATE TABLE department(
        version NUMBER        
 )TABLESPACE users;
 
+CREATE TABLE product(
+       product_id NUMBER PRIMARY KEY,
+       supplier_id NUMBER NOT NULL,
+       warehouse_id NUMBER NOT NULL,
+       product_name VARCHAR2(30) NOT NULL,
+       product_category VARCHAR2(30) NOT NULL,
+       product_size VARCHAR2(20),
+       stock_quantity NUMBER NOT NULL, 
+       measurement_unit VARCHAR2(10),      
+       product_price NUMBER NOT NULL,
+       min_stock_level NUMBER,
+       max_stock_level NUMBER,
+       created_on DATE DEFAULT SYSDATE NOT NULL,
+       last_mod DATE,
+       mod_user VARCHAR2(100),
+       dml_flag VARCHAR2(1),
+       version NUMBER,
+       CONSTRAINT supplier_fk foreign key (supplier_id) references supplier(supplier_id),
+       CONSTRAINT warehouse_fk foreign key (warehouse_id) references warehouse(warehouse_id),
+       CONSTRAINT quantity_positive_check CHECK (stock_quantity > 0),
+       CONSTRAINT price_positive_check CHECK (product_price > 0),
+       CONSTRAINT min_stock_positive_check CHECK (min_stock_level > 0),
+       CONSTRAINT stock_level_check CHECK (max_stock_level > min_stock_level)
+       )TABLESPACE users;
+
 CREATE TABLE warehouse_order(
        wh_order_id NUMBER PRIMARY KEY,
        wh_coordinator_id NUMBER NOT NULL,
@@ -97,7 +96,6 @@ CREATE TABLE warehouse_order(
        constraint wh_order_product_fk foreign key (product_id) references product(product_id),
        constraint wh_quantity_check check (quantity > 0)               
 )TABLESPACE users;
-
 
 CREATE TABLE department_order(
        dept_order_id NUMBER PRIMARY KEY,
