7fa54ae79f127cd1373aad7c655c12594c4d3f43
ArminMolnar <molnar.armin9@gmail.com>
2025-01-06 14:08:31 +0100

    modified test for low_stock procedure

--- Database/15_list_lost_stock_test.sql
+++ Database/15_list_lost_stock_test.sql
@@ -1,10 +1,24 @@
 -- warehouse_seq for warehouse_id starts with 3000     ( There are 3 warehouses stored in the database.)
 -- valid ID
+DECLARE
+  low_stock_l low_stock_list;
 BEGIN
-  pkg_product.list_low_stock(p_warehouse_id => 3002);
+  pkg_product.list_low_stock(p_warehouse_id => 3001, p_low_stock => low_stock_l);
+
+  FOR i IN 1 .. low_stock_l.count
+  LOOP
+    dbms_output.put_line(low_stock_l(i));
+  END LOOP;
 END;
 
--- invalid ID
+-- -- invalid ID
+DECLARE
+  low_stock_l low_stock_list;
 BEGIN
-  pkg_product.list_low_stock(p_warehouse_id => 3005);
+  pkg_product.list_low_stock(p_warehouse_id => 3005, p_low_stock => low_stock_l);
+
+  FOR i IN 1 .. low_stock_l.count
+  LOOP
+    dbms_output.put_line(low_stock_l(i));
+  END LOOP;
 END;
